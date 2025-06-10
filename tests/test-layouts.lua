d8aad91cbcaa52fba11ba1937e2930988b1d8fa7
Michele Lindroos <michele.lindroos@omoroi.fi>
2025-06-10 19:21:00 +0300

    tests: add test for incorrect filetype in layouts

--- tests/test-layouts.lua
+++ tests/test-layouts.lua
@@ -1,20 +1,23 @@
 local layoutsModule = require('autotabpages.layouts')
 
 --_G.vim = require('mock-vim')
-_G.vim = {
-    treesitter = {
-        language = {
-            get_filetypes = spy.new(function(str) return {'.c', '.h'} end),
-        },
-    },
-    list_contains = spy.new(function(tbl, item) return item == "left" or item == "right" end),
-}
 
 describe('Layouts Tests', function()
-    after_each(function()
-        vim.treesitter.language.get_filetypes:clear()
-        vim.list_contains:clear()
+    before_each(function()
+        _G.vim = {
+            treesitter = {
+                language = {
+                    get_filetypes = spy.new(function(str) return {'.c', '.h'} end),
+                },
+            },
+            list_contains = spy.new(function(tbl, item) return item == "left" or item == "right" end),
+        }
     end)
+
+    --after_each(function()
+    --    vim.treesitter.language.get_filetypes:clear()
+    --    vim.list_contains:clear()
+    --end)
 
     it('Test Default Layout Is Valid', function()
         local layouts = layoutsModule.defaults()
@@ -56,5 +59,24 @@ describe('Layouts Tests', function()
         assert.spy(vim.treesitter.language.get_filetypes).was.called_with('c')
         assert.spy(vim.treesitter.language.get_filetypes).was.called_with('cpp')
         assert.spy(vim.list_contains).was.called(11)
+    end)
+
+    it('Test Incorrect Filetype', function()
+        _G.vim.treesitter.language.get_filetypes = function(str) return nil end
+
+        local layouts = {
+            abc = {
+                left = {'*.c'},
+                right = {'*.h'},
+            },
+        }
+
+        local expectedErr = 'Language `abc` is not supported'
+        local result, err = pcall(layoutsModule.verify, layouts)
+
+        assert.is.Not.True(result)
+
+        local actualErr = string.sub(err, -#expectedErr)
+        assert.is.same(actualErr, expectedErr)
     end)
 end)
