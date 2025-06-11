d055ca32b319443eb2cc5e20f566d71b4ab7e27a
Michele Lindroos <michele.lindroos@omoroi.fi>
2025-06-11 18:19:35 +0300

    layouts: split code from main

--- lua/autotabpages/layouts.lua
+++ lua/autotabpages/layouts.lua
@@ -36,4 +36,50 @@ function M.verify(layouts)
     end
 end
 
+function M.findFilesForTabLayout(layout, capture)
+    local result = {}
+
+    for split,patternList in pairs(layout) do
+        result[split] = ""
+
+        for _,pattern in ipairs(patternList) do
+            local filename = string.gsub(pattern, "%*", capture)
+
+            if vim.uv.fs_stat(filename) then
+                result[split] = filename
+                break
+            end
+        end
+
+        if result[split] == "" then
+            return nil
+        end
+    end
+
+    return result
+end
+
+function M.getLayoutMatch(fileFullname, layouts, tabLayoutFunc)
+    for entryName,layout in pairs(layouts) do
+        for split,patternList in pairs(layout) do
+            for _,pattern in ipairs(patternList) do
+                local matcher = string.gsub(pattern, "%*", "(.*)")
+                matcher = "^" .. matcher .. "$"
+
+                local capture = string.match(fileFullname, matcher)
+
+                if capture then
+                    local func = tabLayoutFunc or M.findFilesForTabLayout
+                    local tabLayout = func(layout, capture)
+                    if tabLayout then
+                        return { language = entryName, splits = tabLayout }
+                    end
+                end
+            end
+        end
+    end
+
+    return nil
+end
+
 return M
--- lua/autotabpages/main.lua
+++ lua/autotabpages/main.lua
@@ -1,53 +1,5 @@
 local M = {}
 
-local function filesForAllSplits(layout, capture)
-    local result = {}
-
-    for split,patternList in pairs(layout) do
-        result[split] = ""
-
-        for _,pattern in ipairs(patternList) do
-            local filename = string.gsub(pattern, "%*", capture)
-
-            --if vim.fs.exists(filename) then
-            if vim.uv.fs_stat(filename) then
-                result[split] = filename
-                break
-            end
-        end
-
-        if result[split] == "" then
-            return nil
-        end
-    end
-
-    return result
-end
-
-local function getLayoutMatch(fileFullname)
-    local config = require('autotabpages.config')
-
-    for entryName,layout in pairs(config.options.layouts) do
-        for split,patternList in pairs(layout) do
-            for _,pattern in ipairs(patternList) do
-                local matcher = string.gsub(pattern, "%*", "(.*)")
-                matcher = "^" .. matcher .. "$"
-
-                local capture = string.match(fileFullname, matcher)
-
-                if capture then
-                    local splits = filesForAllSplits(layout, capture)
-                    if splits then
-                        return { language = entryName, splits = splits }
-                    end
-                end
-            end
-        end
-    end
-
-    return nil
-end
-
 function M.newTabLayout(args)
     local filename = args.file
     fileFullname = vim.fs.abspath(filename)
@@ -55,7 +7,9 @@ function M.newTabLayout(args)
     --local basename = vim.fs.basename(args.file)
     --local fullpath = fileFullname:sub(1, #fileFullname-#basename)
 
-    local tabLayout = getLayoutMatch(fileFullname)
+    local config = require('autotabpages.config')
+
+    local tabLayout = require('autotabpages.layouts').getLayoutMatch(fileFullname, config.options.layouts)
     if not tabLayout then
         return
     end
