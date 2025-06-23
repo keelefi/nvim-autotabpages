0e15582ef6a07b6a855409161aa4d357ee252f68
Michele Lindroos <michele.lindroos@omoroi.fi>
2025-06-23 18:53:38 +0300

    verify: unify coding style

--- lua/autotabpages/verify.lua
+++ lua/autotabpages/verify.lua
@@ -1,23 +1,23 @@
 local M = {}
 
 local splits = {
-    "left",
-    "right",
-    "below",
-    "above",
+    'left',
+    'right',
+    'below',
+    'above',
 }
 
 function M.verify(layouts)
     for language,layout in pairs(layouts) do
         local filetypes = vim.treesitter.language.get_filetypes(language)
-        assert(filetypes, string.format("Language `%s` is not supported", language))
+        assert(filetypes, string.format('Language `%s` is not supported', language))
 
         local patterns = {}
         for split,patternList in pairs(layout) do
-            assert(vim.list_contains(splits, split), string.format("Split `%s` is not supported", split))
+            assert(vim.list_contains(splits, split), string.format('Split `%s` is not supported', split))
 
             for _,pattern in ipairs(patternList) do
-                assert(not vim.list_contains(patterns, pattern), string.format("Pattern `%s` found in multiple splits", pattern))
+                assert(not vim.list_contains(patterns, pattern), string.format('Pattern `%s` found in multiple splits', pattern))
 
                 table.insert(patterns, pattern)
             end
